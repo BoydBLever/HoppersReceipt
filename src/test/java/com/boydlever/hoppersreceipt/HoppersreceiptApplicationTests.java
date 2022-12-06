d2339fe4a14cbbd75b8a7efa24a27865c055052d
Boyd Lever <boydlever@stanfordhacker.com>
2022-12-05 17:53:50 -0800

    Initial Commit

---
+++ src/test/java/com/boydlever/hoppersreceipt/HoppersreceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boydlever.hoppersreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersreceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
